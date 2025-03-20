e18fe5347a7576a6181495970f6d50a275000656
MAHKKO <arthur.lionaguiar@mail.com>
2025-03-20 12:25:15 -0300

    mudança na fonte negrito e tamanho

---
+++ alu.py
@@ -0,0 +1,49 @@
+# Nome = int(input('Bem vindo ao mundo da programação, joão '))
+# print(Nome)
+
+# numero01 = int(input('Digite o primeiro numero: '))
+# numero02 = int(input('Digite o segundo numero: '))
+# numero03 = int(input('Digite o terceiro numero: '))
+# soma = numero01*numero02*numero03
+# print(f'O resultado é: {soma}')
+
+# valor_na_carteira = int(input('Digite o valor na carteira: '))
+# cambio = valor_na_carteira/5
+# print(f"O resultado em dolar é de: {cambio}")
+
+# idade = 25
+# nome = 'Ádones'
+# print(f'Meu nome é {nome}, eu tenho {idade} anos')
+
+
+# idade = 23
+# nome = "valdemarques araujo da silva"
+# print(f'meu nome e {nome}, eu tenho {idade} anos')
+
+
+# numero01 = int(input('digite o primeiro numero: '))
+# numero02 = int(input('digite o primeiro numero: '))
+# numero03 = int(input('digite o primeiro numero: '))
+
+
+# soma = numero01 + numero02 + numero03
+
+# print(f'o resultado e: {soma}')
+
+# idade = int(input('Informe sua idade: '))
+
+# if idade < 18:
+#     print('Menor de idade')
+# else:
+#     print('Maior de idade')
+
+
+
+# x = 5
+
+# while (x >= 1):
+#     print (x)
+#     x = x - 1
+# print ("fim do programa")
+
+
